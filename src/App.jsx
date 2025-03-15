039646c62cdbd78a5473d289cf5577dff36b9f73
Yash Jaiswal <architect4646@gmail.com>
2025-03-15 10:51:11 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 
-import Navbar from './Navbar';
-import Footer from './Footer';
-import { WebMaster } from './AllCourses/WebMaster';
+// import Navbar from './Navbar';
+// import Footer from './Footer';
+
+import LoginForm from './Login/LoginForm';
+import Navbar from './Login/Navbar';
+
+
 function App() {
   return (
     <BrowserRouter> 
      <main className="flex flex-col pb-16 overflow-hidden bg-black">
-          <Navbar/>
           
-          <WebMaster />
-          
+          {/* <Navbar/> */}
           
           
+          {/* <Footer /> */}
+
+
+      <Navbar />
+      <section className="flex relative justify-center items-center min-h-[calc(100vh_-_95px)]">
+        <div className="absolute bg-fuchsia-700 blur-[100px] h-[304px] w-[178px] z-[1]" />
+        <LoginForm />
+      </section>
+
           
-          
-          <Footer />
      </main>
     </BrowserRouter>
     
