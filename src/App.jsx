3902f67692187ab00047fc07e29be20f733ad00e
Yash Jaiswal <architect4646@gmail.com>
2025-03-17 22:45:11 +0530

    Completed with my Login pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import AppRoutes from "./routes/AppRoutes";
+// import AppRoutes from "./routes/AppRoutes";
+
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "../src/pages/Login/Navbar";
+import LoginForm from "../src/pages/Login/LoginForm";
 
 
 
 function App() {
   return (
     <>
-   <AppRoutes />;
+   {/* <AppRoutes />; */}
+   <BrowserRouter>
+       <Navbar/>
+      <LoginForm/> 
+   
+   </BrowserRouter>
   </>
 )
 }
