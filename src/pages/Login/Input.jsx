79514059b3e67916a0facd0c14e7af4529b37071
Yash Jaiswal <architect4646@gmail.com>
2025-03-17 23:12:37 +0530

    Edit login

--- src/pages/Login/Input.jsx
+++ src/pages/Login/Input.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const Input = ({ className = "", ...props }) => {
   return (
     <input
-      className={`px-4 py-0 w-full text-xl text-white rounded-full border border-fuchsia-700 border-solid bg-stone-950 h-[60px] max-sm:text-lg max-sm:h-[45px] ${className}`}
+      className={`px-4 py-2 w-[80%] max-w-[300px] text-xl text-white rounded-full 
+                  border border-fuchsia-700 border-solid bg-stone-950 h-[50px] 
+                  max-sm:text-lg max-sm:h-[40px] mx-auto block ${className}`}
       {...props}
     />
   );
